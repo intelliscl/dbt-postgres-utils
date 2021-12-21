4b02ce570d19f3c1c1cf01fd68d3a9fe42b435ae
IS-Josh <48971328+IS-Josh@users.noreply.github.com>
2021-12-21 23:37:28 +1100

    Update uindex.sql

--- macros/optimizers/uindex.sql
+++ macros/optimizers/uindex.sql
@@ -1,5 +1,11 @@
-{% macro uindex(this, column) %}
+-- original credits to https://github.com/fishtown-analytics/postgres
+{% macro uindex(this, column=[],idx_type="btree") %}
 
-create unique index if not exists "{{ this.name }}__uindex_on_{{ column }}" on {{ this }} ("{{ column }}")
+    {% set a_list = [] %}
+    {% set a_list = a_list+column %}
+    {% set csv_string = a_list|join(",") %}
+    {% set text_string = a_list|join ("_")  %}
+
+create unique index if not exists "{{ this.name }}__index_on_{{ text_string }}" on {{ this }} using {{idx_type}} ({{ csv_string }})
 
 {% endmacro %}
