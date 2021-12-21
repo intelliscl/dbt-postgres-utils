a8f5c2d015d8c7d6dd70d00f80e7853e111e2181
IS-Josh <48971328+IS-Josh@users.noreply.github.com>
2021-12-21 23:36:55 +1100

    Update index.sql

--- macros/optimizers/index.sql
+++ macros/optimizers/index.sql
@@ -1,6 +1,11 @@
 -- original credits to https://github.com/fishtown-analytics/postgres
-{% macro index(this, column) %}
+{% macro index(this, column=[],idx_type="btree") %}
 
-create index if not exists "{{ this.name }}__index_on_{{ column }}" on {{ this }} ("{{ column }}")
+    {% set a_list = [] %}
+    {% set a_list = a_list+column %}
+    {% set csv_string = a_list|join(",") %}
+    {% set text_string = a_list|join ("_")  %}
+
+create index if not exists "{{ this.name }}__index_on_{{ text_string }}" on {{ this }} using {{idx_type}} ({{ csv_string }})
 
 {% endmacro %}
